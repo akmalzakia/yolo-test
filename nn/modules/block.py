367b22743d08faa56ec8e3c3073162ab7cf560a6
akmalzakia <zakiasmara11@gmail.com>
2026-05-06 15:50:12 +0700

    adjust SimAM to fp32

--- nn/modules/block.py
+++ nn/modules/block.py
@@ -2095,15 +2095,15 @@ class SimAM(torch.nn.Module):
         return "simam"
 
     def forward(self, x):
-
-        b, c, h, w = x.size()
+        x_fp32 = x.float()
         
-        n = w * h - 1
+        var = x_fp32.var(dim=[2, 3], keepdim=True, unbiased=True)
+        mu = x_fp32.mean(dim=[2, 3], keepdim=True)
+        
+        x_minus_mu_square = (x_fp32 - mu).pow(2)
+        y = x_minus_mu_square / (4 * (var + self.e_lambda)) + 0.5
 
-        x_minus_mu_square = (x - x.mean(dim=[2,3], keepdim=True)).pow(2)
-        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2,3], keepdim=True) / n + self.e_lambda)) + 0.5
-
-        return x * self.activaton(y)
+        return x * self.activaton(y.to(x.dtype))
 
 class ShapeConv(nn.Module):
 
